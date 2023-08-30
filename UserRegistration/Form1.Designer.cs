87c4df723cc7d330f411b2429578bb6ea9bfc9d4
maryam <maryam.anaraki@gmail.com>
2023-08-30 11:43:01 +0330

    Add project files.

---
+++ UserRegistration/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace UserRegistration
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.phone = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.email = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.last_name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.submit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.first_name = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // phone
+            // 
+            this.phone.Location = new System.Drawing.Point(198, 378);
+            this.phone.Name = "phone";
+            this.phone.Size = new System.Drawing.Size(365, 31);
+            this.phone.TabIndex = 3;
+            this.phone.Validating += new System.ComponentModel.CancelEventHandler(this.phone_Validating);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label5.Location = new System.Drawing.Point(193, 350);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 29);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Phone:";
+            // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(198, 290);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(365, 31);
+            this.email.TabIndex = 2;
+            this.email.Validating += new System.ComponentModel.CancelEventHandler(this.email_Validating);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label4.Location = new System.Drawing.Point(193, 262);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 29);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Email:";
+            // 
+            // last_name
+            // 
+            this.last_name.Location = new System.Drawing.Point(198, 202);
+            this.last_name.Name = "last_name";
+            this.last_name.Size = new System.Drawing.Size(365, 31);
+            this.last_name.TabIndex = 1;
+            this.last_name.Validating += new System.ComponentModel.CancelEventHandler(this.last_name_Validating);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(193, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Last Name:";
+            // 
+            // submit
+            // 
+            this.submit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submit.Location = new System.Drawing.Point(320, 463);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(112, 41);
+            this.submit.TabIndex = 4;
+            this.submit.Text = "Submit";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(191, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(370, 37);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "User Registration Form";
+            // 
+            // first_name
+            // 
+            this.first_name.Location = new System.Drawing.Point(198, 118);
+            this.first_name.Name = "first_name";
+            this.first_name.Size = new System.Drawing.Size(365, 31);
+            this.first_name.TabIndex = 0;
+            this.first_name.Validating += new System.ComponentModel.CancelEventHandler(this.first_name_Validating);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(193, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "First Name:";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 573);
+            this.Controls.Add(this.phone);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.last_name);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.first_name);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Registration";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox phone;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox last_name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox first_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ UserRegistration/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace UserRegistration
+{
+    public partial class Form1 : Form
+    {
+        //Declare an object of UserInfoEntities
+        UserInfoEntities _db;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Instantiate database
+            _db = new UserInfoEntities();
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            // save user information in database
+            SaveUser();
+            
+            MessageBox.Show("successful registration");                        
+        }        
+
+        private void SaveUser()
+        {
+            User user = new User();
+            user.first_name = first_name.Text.Trim();
+            user.last_name = last_name.Text.Trim();
+            user.email = email.Text.Trim();
+            user.phone = phone.Text.Trim();
+            _db.Users.Add(user);
+            _db.SaveChanges();
+        }
+            
+        private void phone_Validating(object sender, CancelEventArgs e)
+        {
+            string errorMsg;
+
+            if (!ValidPhoneNumber(phone.Text.Trim(), out errorMsg))
+            {
+                // Cancel the event and select the text to be corrected by the user.
+                e.Cancel = true;
+                phone.Select(0, phone.Text.Length);
+
+                // Set the ErrorProvider error with the text to display. 
+                this.errorProvider1.SetError(phone, errorMsg);
+            }            
+        }
+
+        public bool ValidPhoneNumber(string phoneNumber, out string errorMessage)
+        {
+            Regex re = new Regex("^9[0-9]{9}");
+
+            // Confirm that the phone number string is not empty.
+            if (phoneNumber.Length == 0)
+            {
+                errorMessage = "field is required.";
+                return false;
+            }
+
+            // Confirm that phone number is valid.
+            if (phoneNumber.Length == 10)
+            {
+                if (re.IsMatch(phoneNumber))
+                {
+                    errorMessage = "";
+                    return true;
+                }
+            }            
+
+            errorMessage = "phone number must be 10-digit and starts with 9";
+            return false;
+        }
+
+        private void email_Validating(object sender, CancelEventArgs e)
+        {
+            string errorMsg;
+
+            if (!ValidEmailAddress(email.Text, out errorMsg))
+            {
+                // Cancel the event and select the text to be corrected by the user.
+                e.Cancel = true;
+                email.Select(0, email.Text.Length);
+
+                // Set the ErrorProvider error with the text to display. 
+                this.errorProvider1.SetError(email, errorMsg);
+            }            
+        }
+
+        public bool ValidEmailAddress(string emailAddress, out string errorMessage)
+        {
+            // Confirm that the email address string is not empty.
+            if (emailAddress.Length == 0)
+            {
+                errorMessage = "field is required.";
+                return false;
+            }
+
+            // Confirm that there is an "@" and a "." in the email address, and in the correct order.
+            if (emailAddress.IndexOf("@") > -1)
+            {
+                if (emailAddress.IndexOf(".", emailAddress.IndexOf("@")) > emailAddress.IndexOf("@"))
+                {
+                    errorMessage = "";
+                    return true;
+                }
+            }
+
+            errorMessage = "email address must be in valid format.\n" +
+               "For example 'someone@example.com' ";
+            return false;
+        }
+
+        private void last_name_Validating(object sender, CancelEventArgs e)
+        {
+            string errorMsg;
+
+            // Confirm that the last name string is not empty.
+            if (last_name.Text.Length == 0)
+            {
+                // Cancel the event and select the text to be corrected by the user.
+                e.Cancel = true;
+                first_name.Select(0, last_name.Text.Length);
+
+                // Set the ErrorProvider error with the text to display. 
+                errorMsg = "field is required!";
+                errorProvider1.SetError(last_name, errorMsg);
+            }
+        }
+
+        private void first_name_Validating(object sender, CancelEventArgs e)
+        {
+            string errorMsg;
+
+            // Confirm that the first name string is not empty.
+            if (first_name.Text.Length == 0)
+            {
+                // Cancel the event and select the text to be corrected by the user.
+                e.Cancel = true;
+                first_name.Select(0, first_name.Text.Length);
+
+                // Set the ErrorProvider error with the text to display. 
+                errorMsg = "field is required!";
+                errorProvider1.SetError(first_name, errorMsg);
+            }           
+        }  
+    }
+}
